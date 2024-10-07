3e683ab74eaed9844b2fa50882384dd22d0bec9f
Streamlit20 <gurukiranm2010@gmail.com>
2024-10-07 12:16:23 +0530

    Update Chat.py

--- Chat.py
+++ Chat.py
@@ -5,7 +5,7 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
